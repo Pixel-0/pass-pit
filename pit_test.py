f1f3e1ee177397911d889165fb894e8ab87c4ee0
Pixel-0 <tiny0pixel@gmail.com>
2019-07-21 23:48:16 +0300

    run test for new user

--- pit_test.py
+++ pit_test.py
@@ -33,3 +33,33 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_credential.username,"@pixel")
         self.assertEqual(self.new_credential.email,"pixel@live.com")
         self.assertEqual(self.new_credential.password,"#####")
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the user class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_user = User("Aurora","Pixel","@tello","pixel@live.com","#####")
+
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            credential_list = []
+        
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+        self.assertEqual(self.new_user.first_name,"Aurora")
+        self.assertEqual(self.new_user.last_name,"Pixel")
+        self.assertEqual(self.new_user.username,"@tello")
+        self.assertEqual(self.new_user.email,"pixel@live.com")
+        self.assertEqual(self.new_user.password,"#####")
